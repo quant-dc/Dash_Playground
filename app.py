2c6e43ed903caa35f1eb4602bc234331ad6846f5
David Callow <davidc.callow@btinternet.com>
2017-12-11 19:58:34 +0000

    INITIAL COMMIT

---
+++ app.py
@@ -0,0 +1,39 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+
+my_css_url = []
+
+for css in my_css_url:
+    app.css.append_css({
+        "external_url": css})
+
+app.layout = html.Div(children=[
+    html.Div([
+        html.Div(['Hello'], className='logo')
+    ], className='navbar'),
+    html.Div([
+        html.H1(children='Hello Dash'),
+        html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
+    ]),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
